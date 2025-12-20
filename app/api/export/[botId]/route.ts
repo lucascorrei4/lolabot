806ec3682acf7ff1afcfcd73de2e9f3430bbb269
lucascorrei4 <lucascorreiaevangelista@gmail.com>
2025-12-20 00:12:02 -0600

    fix: correct import path in export route

--- app/api/export/[botId]/route.ts
+++ app/api/export/[botId]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { listSessions, listMessages } from "../../../../../lib/db/mongo";
+import { listSessions, listMessages } from "../../../../lib/db/mongo";
 
 export async function GET(
     request: NextRequest,
